b8e9a66cb81878a678d26f1445804c6f545542ba
ViniciusMatee3e <144918311+ViniciusMatee3e@users.noreply.github.com>
2023-12-05 13:18:27 -0300

    Create nascimento.py

---
+++ nascimento.py
@@ -0,0 +1,6 @@
+ano_nascimento = int(input("Digite o ano de nascimento: "))
+ano_atual = int(input("Digite o ano atual: "))
+
+idade = ano_atual - ano_nascimento
+
+print(f"Sua idade é {idade} anos.")
